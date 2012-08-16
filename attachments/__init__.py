bfc3db4fa035a9729f15401e95f1f5d0d114395b
Marinho Brandao <marinho@gmail.com>
2012-08-16 10:16:57 -0300

    Using new London's AppSettings and implementing basic code

---
+++ attachments/__init__.py
@@ -0,0 +1,6 @@
+from london.conf import AppSettings
+
+app_settings = AppSettings()
+
+app_settings.declare('ATTACHABLE_MODELS', default=None)
+
--- attachments/models.py
+++ attachments/models.py
@@ -1,15 +1,17 @@
 from london.db import models
 
+from attachments import app_settings
 
 class DocumentQuerySet(models.QuerySet):
     def attached_to(self, obj):
-        # TODO
-        pass
+        # TODO: this code is duplicating requests to the database and must be improved to do it only once
+        attacheds_pks = [doc['pk'] for doc in DocumentAttachment.query().filter(attached_to=obj).values_list('document',flat=True)]
+        return self.filter(pk__in=attacheds_pks)
 
 
 class Document(models.Model):
     class Meta:
-        query = 'faq.models.DocumentQuerySet'
+        query = 'attachments.models.DocumentQuerySet'
 
     title = models.CharField(max_length=100, db_index=True)
     tags = models.ListField(blank=True, null=True, db_index=True)
@@ -20,18 +22,11 @@ class Document(models.Model):
         return self['title']
 
     def attach_to(self, to):
-        pass # TODO
+        """Creates an attachment from this document to the given object."""
+        self['attachments'].get_or_create(attached_to=to)
 
 
 class DocumentAttachment(models.Model):
     document = models.ForeignKey('Document', related_name='attachments')
-    attached_to = models.ForeignKey()
-
-# SIGNALS
-
-from london.db import signals
-
-def post_delete(instance, **kwargs):
-    pass # TODO - consider only declared model classes for performance reasons
-signals.post_delete.connect(post_delete)
+    attached_to = models.ForeignKey(delete_cascade=True, db_index=True)
 
